c86b89b07d6b5b1cff3be120b2ea4bae6bce2553
Nicholas Salvatore <nmsalvatore@gmail.com>
2019-07-26 20:39:29 -0700

    ui redesign

--- workout-generator/app.js
+++ workout-generator/app.js
@@ -39,22 +39,70 @@ const rehab = {
   ],
 }
 
-function displayWorkout() {
+const mobility = [
+    {type: 'bar hang', reps: 5},
+    {type: 'improved cobra', reps: 5},
+    {type: 'side bend', reps: 5},
+    {type: 'spine rotation', reps: 5},
+    {type: 'later neck & trap stretch', reps: 5},
+    {type: 'headache buster', reps: 5},
+    {type: 'head turner', reps: 5},
+    {type: 'chest opener', reps: 5},
+    {type: 'overhead reach', reps: 5},
+    {type: 'biceps & shoulder stretch', reps: 5},
+    {type: 'shoulder blade & lat stretch', reps: 5},
+    {type: 'upper back loosener', reps: 5},
+    {type: 'wrist flexion', reps: 5},
+    {type: 'wrist extension', reps: 5},
+    {type: 'good morning hamstring stretch', reps: 5},
+    {type: 'kneeling hip flexor stretch', reps: 5},
+    {type: 'lunge hip flexor stretch', reps: 5},
+    {type: 'karate stance hip flexor stretch', reps: 5},
+    {type: 'karate stance groin stretch', reps: 5},
+    {type: 'seated groin stretch', reps: 5},
+    {type: 'calf stretch', reps: 5},
+    {type: 'shin and instep stretch', reps: 5}
+  ]
+
+function displayWorkout(movementGroup) {
+
+// Add a function that displays movements based on what tab is active.
+
+  const navUl = document.querySelector('.navbar .collection');
+
+  for (let i = 0; i < 4; i++) {
+    if (navUl.children[i].classList.contains('active')) {
+      movementGroup = navUl.children[i].textContent;
+    } 
+  }
+
   const collection = document.getElementById('movement-collection');
   collection.innerHTML = null;
 
-  createNewList(rehab.neck);
-  createNewList(rehab.wrists);
-  createNewList(getRandomMovements(rehab.back, 4));
-  createNewList(getRandomMovements(rehab.core, 3));
-  createNewList(getRandomMovements(rehab.chest, 1));
-  createNewList(getRandomMovements(rehab.hips, 4));
+  // for (let key in rehab) {
+  //   createNewList( rehab[key] );
+  // }
+
+  switch (movementGroup) {
+    case 'rehab':
+      createNewList(rehab.neck);
+      createNewList(rehab.wrists);
+      createNewList(getRandomMovements(rehab.back, 3));
+      createNewList(getRandomMovements(rehab.core, 2));
+      createNewList(getRandomMovements(rehab.chest, 1));
+      createNewList(getRandomMovements(rehab.hips, 3));
+      break;
+    case 'mobility':
+      createNewList(getRandomMovements(mobility, 5));
+  }
 
   function createNewList(movementType) {
     const movementUl = document.createElement('ul');
+    movementUl.className = 'collection';
 
     movementType.forEach(function createListItems(movement) {
       const movementLi = document.createElement('li');
+      movementLi.className = 'collection-item';
      
       (typeof movement.reps === 'number') ?
         movementLi.textContent = `${movement.reps}x, ${movement.type}` :
@@ -85,7 +133,7 @@ function getRandomMovements(movementGroup, numberOfMovements) {
 }
 
 function toggleActivityClass() {
-  const navUl = document.querySelector('.nav-ul');
+  const navUl = document.querySelector('.navbar .collection');
 
   for (let i = 0; i < 4; i++) {
     if (navUl.children[i].classList.contains('active')) {
@@ -95,18 +143,17 @@ function toggleActivityClass() {
 }
 
 function loadEventListeners() {
-  document.addEventListener('DOMContentLoaded', displayWorkout);
+  document.addEventListener('DOMContentLoaded', displayWorkout('rehab'));
 
-  const navUl = document.querySelector('.nav-ul');
+  const navUl = document.querySelector('.navbar .collection');
   navUl.addEventListener('click', selectCollectionType);
 
   function selectCollectionType(e) {
-    const type = e.target.id.slice(0, -4);
+    toggleActivityClass();
+    e.target.classList.add('active');
 
-    if (rehab[type]) {
-      toggleActivityClass();
-      e.target.className = 'active';
-    }
+    const movementGroup = e.target.textContent;
+    displayWorkout(movementGroup);
   }
 
   const redoBtn = document.getElementById('redo-btn');
