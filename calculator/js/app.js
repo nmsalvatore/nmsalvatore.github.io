96d4608f47527712ed768e0bff04f29117d7feda
Nicholas Salvatore <nmsalvatore@gmail.com>
2019-08-24 17:32:13 -0700

    Added owed tax and tip

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -3,18 +3,26 @@ const subtotal = document.getElementById('subtotal');
 const total = document.getElementById('total');
 const tip = document.getElementById('tip');
 const form = document.querySelector('.form');
+
 const displayTotal = document.getElementById('displayTotal');
+const displayTax = document.getElementById('displayTax');
+const displayTip = document.getElementById('displayTip');
 
 const calculate = () => {
   let percentage = purchases.value / subtotal.value;
   let totalOwedNoTip = total.value * percentage;
+  let taxOwed = totalOwedNoTip - purchases.value;
   let tipOwed = totalOwedNoTip * (tip.value * .01);
   let totalOwed = totalOwedNoTip + tipOwed;
 
-  if (isNaN(totalOwed)) {
+  if (isNaN(totalOwed) || taxOwed < 0) {
     displayTotal.innerText = '$0.00';
+    displayTax.innerText = 'Tax: $0.00';
+    displayTip.innerText = 'Tip: $0.00';
   } else {
-    displayTotal.innerText =  `$${totalOwed.toFixed(2)}`;
+    displayTotal.innerText = `$${totalOwed.toFixed(2)}`;
+    displayTax.innerText = `Tax: $${taxOwed.toFixed(2)}`;
+    displayTip.innerText = `Tip: $${tipOwed.toFixed(2)}`;
   }
 }
 
